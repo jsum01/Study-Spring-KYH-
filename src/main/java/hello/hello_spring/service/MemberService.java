ee75c516dc36bffe4358d0b85a314e43cd312b08
jsum01 <djadjawltjr@gmail.com>
2024-07-09 13:58:18 +0900

    [Create] Member Controller

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -45,7 +45,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findOne(Member member) {
-        return memberRepository.findById(member.getId());
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
